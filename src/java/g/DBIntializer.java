a2c922d679e40bba492a867a9c81a1257d1537b7
Mudit Bhartia <muditbhartia@gmail.com>
2019-03-28 15:08:05 +0530

    OnlineBanking
    Netbeans Code

---
+++ src/java/g/DBIntializer.java
@@ -0,0 +1,8 @@
+package g;
+
+public interface DBIntializer {
+String DRIVER="org.apache.derby.jdbc.ClientDriver";
+String CON_STRING="jdbc:derby://localhost:1527/onlinebanking";
+String USERNAME="app";
+String PASSWORD="app";
+}
